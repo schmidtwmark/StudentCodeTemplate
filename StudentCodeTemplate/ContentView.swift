527f6e916946f3b1462b2993502a8f3e5647a147
Mark Schmidt <mark.schmidt@hey.com>
2024-11-14 13:00:23 -0600

    Initial Commit

---
+++ StudentCodeTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudentCodeTemplate
+//
+//  Created by Mark Schmidt on 11/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudentCodeTemplate/StudentCodeTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentCodeTemplateApp.swift
+//  StudentCodeTemplate
+//
+//  Created by Mark Schmidt on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct StudentCodeTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
